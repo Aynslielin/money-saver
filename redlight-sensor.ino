0c184a3de1681d562f0cbe188773db54391473c3
Aynslielin <161717296+Aynslielin@users.noreply.github.com>
2025-01-09 14:26:36 +0800

    Add files via upload

---
+++ redlight-sensor.ino
@@ -0,0 +1,24 @@
+int val;
+void setup() {
+// put your setup code here, to run once:
+// Serial.begin(9600);
+pinMode(A1, INPUT);
+pinMode(12, OUTPUT);//藍色LED
+pinMode(13,OUTPUT);//黃色LED
+}
+
+void loop() 
+{
+  val = analogRead(A1);
+  // Serial.println(val);
+  if(val < 50)           //硬幣未通過時亮藍燈
+  {
+  digitalWrite(12, HIGH);//藍色亮
+  digitalWrite(13, LOW);//黃色暗
+  }else                 //硬幣通過時亮黃燈
+  {
+  digitalWrite(12, LOW);//藍色暗
+  digitalWrite(13, HIGH);//黃色亮
+  delay(5000);//硬幣通過時延遲5秒
+  }
+}
